d384f5e36085ae292be40a5e0089bc633972be59
RossRekin <markixbg@gmail.com>
2020-10-01 16:20:04 +0300

    awk solutions added

--- more-scripts/1-url-requests.sh
+++ more-scripts/1-url-requests.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 
 LOGS_FILE=$1
-declare -A urls
+#declare -A urls
 
 
 #exercise 1
 
 #fill the array
-while read LINE; do
-  elements=($LINE);
-  URL="${elements[10]}";
-  if [ "${URL}" != '"-"' -a -n "${URL}" ]; then
-    if [ ${urls[$URL]} ]; then
-      COUNT=$((${urls[$URL]} +1))
-      urls[$URL]=$COUNT
-      #echo "${URL} -> ${urls[$URL]}"
-      else 
-        urls[$URL]=1
-    fi
-  fi
+#while read LINE; do
+#  elements=($LINE);
+#  URL="${elements[10]}";
+#  if [ "${URL}" != '"-"' -a -n "${URL}" ]; then
+#    if [ ${urls[$URL]} ]; then
+#      COUNT=$((${urls[$URL]} +1))
+#      urls[$URL]=$COUNT
+#      #echo "${URL} -> ${urls[$URL]}"
+#      else 
+#        urls[$URL]=1
+#    fi
+#  fi
+#
+#done < $LOGS_FILE
+#
+##print sorted
+#for h in "${!urls[@]}"
+#do
+#    printf "%5s %10s \n" ${urls["$h"]} ${h}
+#    #echo ${urls["$h"]} ' ' ${h} 
+#done | sort -rn -k1 | head -n 10
 
-done < $LOGS_FILE
-
-#print sorted
-for h in "${!urls[@]}"
-do
-    printf "%5s %10s \n" ${urls["$h"]} ${h}
-    #echo ${urls["$h"]} ' ' ${h} 
-done | sort -rn -k1 | head -n 10
-
-
+#awk solution
+awk '{ print $11}' $LOGS_FILE | sort | uniq -c | sort -nr | head -n 10 | column -t
--- more-scripts/2-top-visitors.sh
+++ more-scripts/2-top-visitors.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
 LOGS_FILE=$1
-declare -A visitors
+#declare -A visitors
+#
+#while read LINE; do
+#  elements=($LINE);
+#  VISITOR="${elements[0]}";
+#  if [ -n "${VISITOR}" ]; then
+#    if [ ${visitors[$VISITOR]} ]; then
+#      COUNT=$((${visitors[$VISITOR]} +1))
+#      visitors[$VISITOR]=$COUNT
+#      else 
+#        visitors[$VISITOR]=1
+#    fi
+#  fi
+#done < $LOGS_FILE
+#
+#for v in "${!visitors[@]}"
+#do
+#    printf "%5s %10s \n" ${visitors["$v"]} ${v} 
+#done | sort -rn -k1 | head -n 10
 
-while read LINE; do
-  elements=($LINE);
-  VISITOR="${elements[0]}";
-  if [ -n "${VISITOR}" ]; then
-    if [ ${visitors[$VISITOR]} ]; then
-      COUNT=$((${visitors[$VISITOR]} +1))
-      visitors[$VISITOR]=$COUNT
-      else 
-        visitors[$VISITOR]=1
-    fi
-  fi
-done < $LOGS_FILE
-
-for v in "${!visitors[@]}"
-do
-    printf "%5s %10s \n" ${visitors["$v"]} ${v} 
-done | sort -rn -k1 | head -n 10
+#awk solution
+awk '{ print $1}' $LOGS_FILE | sort | uniq -c | sort -nr | head -n 10 | column -t
